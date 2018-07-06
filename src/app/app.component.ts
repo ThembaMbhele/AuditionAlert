b5e42a095a2ebec75460c03ba30aa4e8aa607d9e
CalvinMogodi <Calvin.Mogodi@eoh.co.za>
2018-07-06 17:03:48 +0200

    images

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,7 +16,7 @@ import { LoginPage } from '../pages/login/login';
 export class MyApp {
   @ViewChild(Nav) nav: Nav;
 
-  rootPage: any = LoginPage;
+  rootPage: any = DashboardPage;
 
   pages: Array<{title: string, component: any}>;
 
