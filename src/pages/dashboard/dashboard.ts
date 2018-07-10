1b011aad2ba3813369858a7d923062c18027236a
CalvinMogodi <Calvin.Mogodi@eoh.co.za>
2018-07-10 09:20:34 +0200

    last fixes

--- src/pages/dashboard/dashboard.ts
+++ src/pages/dashboard/dashboard.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams , ToastController} from 'ionic-angular';
+import { NavController, NavParams, ToastController } from 'ionic-angular';
 import { UploadeventPage } from '../uploadevent/uploadevent';
 import { AuditionProvider } from '../../providers/audition/audition';
 import { GlobalVariablesProvider } from '../../providers/global-variables/global-variables';
 import { HttpClient } from '@angular/common/http';
-import { Storage } from '@ionic/storage'; 
+import { Storage } from '@ionic/storage';
 
 /**
  * Generated class for the DashboardPage page.
@@ -20,19 +20,19 @@ import { Storage } from '@ionic/storage';
 export class DashboardPage {
   public showSlides: boolean = false;
   public auditions: any[];
-  public userType: string ;
+  public userType: string;
 
   constructor(public navCtrl: NavController, public navParams: NavParams, public http: HttpClient, public storage: Storage, public auditionProvider: AuditionProvider, public toastCtrl: ToastController, private globalVariables: GlobalVariablesProvider) {
     //get audition events
     this.storage.get('userType').then((val) => {
-        if (val) {
-          this.userType = val;
-        }
+      if (val) {
+        this.userType = val;
+      }
     });
     this.getAuditions();
   }
 
-  getAuditions(){
+  getAuditions() {
     this.auditionProvider.getAuditions().
       subscribe((response: any[]) => {
         this.auditions = response;
@@ -47,9 +47,9 @@ export class DashboardPage {
   }
 
   toTimestamp(strDate) {
-        var datum = Date.parse(strDate);
-        return datum / 1000;
-    }
+    var datum = Date.parse(strDate);
+    return datum / 1000;
+  }
 
   timeConverter(datetime) {
     var months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
@@ -68,28 +68,31 @@ export class DashboardPage {
     this.navCtrl.push(UploadeventPage);
   }
 
-  openURL(URL) {
-    window.open(URL, '_system', 'location=yes');
+  openURL(url) {
+    if (!/^(f|ht)tps?:\/\//i.test(url)) {
+      url = "http://" + url;
+    }
+    window.open(url, '_blank', 'location=no');
   }
 
   deleteAuditions(audition) {
-     this.http.post("http://197.242.149.23/api/deleteAudition", {auditionId : audition.auditionId}).subscribe((response: any) => {
-       if(response){
-         this.showMessage('Event has been deleted successfull');
-         this.getAuditions();
-       }else{
+    this.http.post("http://197.242.149.23/api/deleteAudition", { auditionId: audition.auditionId }).subscribe((response: any) => {
+      if (response) {
+        this.showMessage('Event has been deleted successfull');
+        this.getAuditions();
+      } else {
         this.showMessage('We are unable to deleted event please try again later.');
-       }
-     });
+      }
+    });
   }
 
-  showMessage(message){
-     let toast = this.toastCtrl.create({
-                        message: message,
-                        duration: 2000,
-                        position: 'bottom'
-                    });
-                    toast.present(toast);
+  showMessage(message) {
+    let toast = this.toastCtrl.create({
+      message: message,
+      duration: 2000,
+      position: 'bottom'
+    });
+    toast.present(toast);
   }
 
 }
